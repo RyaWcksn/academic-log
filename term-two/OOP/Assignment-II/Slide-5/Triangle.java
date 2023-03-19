9c90d9486d7145be82410dfd0682a74b5e454057
RyaWcksn <pram.aryawcksn@protonmail.ch>
2023-03-19 23:44:28 +0700

    Update

---
+++ term-two/OOP/Assignment-II/Slide-5/Triangle.java
@@ -0,0 +1,17 @@
+class Triangle {
+    public static void main(String args[]) {
+        for (int i=1; i <= 6; i++) {
+
+            for (int j= 6-i; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+
+            for (int k = (2 * i - 1); k >= 1; k--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+Footer
